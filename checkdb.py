b1c781b07ac9ef9426c4b5ef015dd6ce83730c44
legovn <android.r2n2@gmail.com>
2017-07-12 05:42:55 +0300

    Combining TS, OS, RLS

--- checkdb.py
+++ checkdb.py
@@ -1,27 +1,38 @@
 '''
-
-Check occurrences Drug1-Drug2 pairs in DrugBank and Twosides
-
+Extracting AE/Interactions from Twosides
+Extracting AE from Offsides
 '''
 
-#DrugBank
-def get_interaction(drug1, drug2):
-    if drug1 in list(dct_interact.keys()):
-        if drug2 in list(dct_interact[drug1].keys()):
-            result = dct_interact[drug1][drug2]
-            return result
-        else:
-            #print(drug1 + ' does not interact with ' + drug2)
-            return None         
+
+#Extracting AE/Interactions from Twosides
+def get_ae_interaction(drug1, drug2):
+    tpl_keys = tuple([drug1, drug2].sort())
+    if tpl_keys in list(dct_DB_TS.keys()):
+        return dct_DB_TS[tpl_keys] 
     else:
-        #print(drug1 + ' not in DrugBank')
         return None
 
-def get_ae(drug1, drug2):
-    twosides_subset = twosides[(twosides['drug1'] == drug1) & (twosides['drug2'] == drug2) |
-        (twosides['drug2'] == drug1) & (twosides['drug1'] == drug2)]
-    if twosides_subset.empty:
-        return None
+def get_interaction(drug1, drug2):
+    tpl_keys = tuple([drug1, drug2].sort())
+    if tpl_keys in list(dct_DB_TS.keys()):
+        return dct_DB_TS[tpl_keys][1] 
     else:
-        dct = dict(zip( twosides_subset['event_umls_id'], twosides_subset['proportional_reporting_ratio'].round(2) ))
-        return dct
+        return None
+    
+def get_ae(drug1, drug2):
+    tpl_keys = tuple([drug1, drug2].sort())
+    if tpl_keys in list(dct_DB_TS.keys()):
+        return dct_DB_TS[tpl_keys][0] 
+    else:
+        return None    
+    
+#Extracting AE from Offsides
+def get_ae_offsides(drug):
+    if drug in list(set(offsides['drug'])):
+    #if drug in lst_drugs_offsides:
+        offsides_subset = offsides[offsides['drug']==drug]
+        dct_drug_extract_offsides = dict(zip( offsides_subset['umls_id'], list(zip(offsides_subset['rr'].round(2), offsides_subset['pvalue'])) ))
+        return dct_drug_extract_offsides
+    else:
+        #print(drug + ' has no reported AE in Offsides')
+        return None
